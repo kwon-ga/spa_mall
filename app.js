86b1c40a8b5c34e48849187ff32eea57779347a0
kwon-ga <rnjsdydry@gmail.com>
2022-10-05 11:31:05 +0900

    개인과제 제출

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//
+const express = require('express')
+const app = express()
+const port = 3000 
+const indexroutes = require('./routes/index')
+
+const connect = require('./schemas/index')
+connect();
+
+
+
+app.use(express.json());
+app.use('/',indexroutes);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
