4b5ec82b64b884d749414ae2cbb09bafd2973028
kwon-ga <112460494+kwon-ga@users.noreply.github.com>
2022-10-11 12:55:01 +0900

    Delete schemas directory

--- schemas/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connect = () => {
-    mongoose.connect('mongodb://localhost:27017/spa_board').catch(err => console.log(err));
-}
-
-mongoose.connection.on("error",err => { console.log('err!!'+err)});
-
-module.exports = connect;
